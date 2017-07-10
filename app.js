51cc60362080f11e8c0d68d069f5c0bfde69887d
Rob Currie <rcurrie@ucsc.edu>
2017-07-10 23:04:11 +0000

    Fully functional with object tagging and json output, though very very much hack code

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const s3DemoGlobals = {};
 
 // eslint-disable-next-line no-unused-vars
-function loginToGoogle(response) {
+function signIn(response) {
   const profile = response.getBasicProfile();
   s3DemoGlobals.email = profile.getEmail();
   s3DemoGlobals.name = profile.getName();
+  console.log(s3DemoGlobals.email, s3DemoGlobals.name);
+
+  $("#name").html(s3DemoGlobals.name);
+  $("#email").html(s3DemoGlobals.email);
   s3DemoGlobals.assumeRoleWithWebIdentity({
     roleArn: "arn:aws:iam::238605363322:role/receiving-browser-role",
     idToken: response.getAuthResponse().id_token,
@@ -12,16 +16,20 @@ function loginToGoogle(response) {
   $("#fine-uploader-s3").show();
 }
 
+function signOut() {
+  $("#fine-uploader-s3").hide();
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(console.log("User signed out."));
+}
+
 $(function() {
   var assumeRoleWithWebIdentity = function(params) {
-    var sts = new AWS.STS(),
-      assumeRoleParams = {};
 
     s3DemoGlobals.roleArn = params.roleArn || s3DemoGlobals.roleArn;
     s3DemoGlobals.providerId = params.providerId || s3DemoGlobals.providerId;
     s3DemoGlobals.idToken = params.idToken || s3DemoGlobals.idToken;
 
-    assumeRoleParams = {
+    let assumeRoleParams = {
       RoleArn: s3DemoGlobals.roleArn,
       RoleSessionName: "web-identity-federation",
       WebIdentityToken: s3DemoGlobals.idToken
@@ -31,7 +39,9 @@ $(function() {
       assumeRoleParams.ProviderId = s3DemoGlobals.providerId;
     }
 
+    let sts = new AWS.STS();
     sts.assumeRoleWithWebIdentity(assumeRoleParams, params.callback || s3DemoGlobals.updateCredentials);
+    s3DemoGlobals.sts = sts;
   },
     getFuCredentials = function(data) {
       return {
@@ -52,6 +62,16 @@ $(function() {
       if (!error) {
         $('#fine-uploader-s3').fineUploaderS3("setCredentials", s3DemoGlobals.getFuCredentials(data));
       }
+      console.log(data.Credentials);
+      AWS.config.update({
+          credentials: {
+            // region: data.Credentials.Region,
+            // region: 'us-west-2',
+            accessKeyId: data.Credentials.AccessKeyId,
+            secretAccessKey: data.Credentials.SecretAccessKey,
+            sessionToken: data.Credentials.SessionToken
+          }
+      });
     },
     hideUploader = function() {
       $("#fine-uploader-s3").hide();
@@ -59,19 +79,25 @@ $(function() {
 
   $("#fine-uploader-s3").fineUploaderS3({
     request: {
-      endpoint: bucketUrl
+      endpoint: bucketUrl,
+      // these are undefined at this point but should fill in just in case tags get lost
+      // params: {
+      //   email: s3DemoGlobals.email,
+      //   name: s3DemoGlobals.name,
+      // },
     },
     objectProperties: {
       acl: "private",
 
       // The key for each file will follow this format: {USER_NAME}/{UUID}.{FILE_EXTENSION}
       key: function(id) {
-        var filename = this.getName(id),
-          uuid = this.getUuid(id);
+        var filename = this.getName(id), size = this.getSize(id);
+        var uuid = getUUIDByString(s3DemoGlobals.email + filename + size.toString());
 
         // return qq.format("{}/{}.{}.{}", s3DemoGlobals.userName, filename, uuid, qq.getExtension(filename));
-        return qq.format("{}/{}", s3DemoGlobals.email, filename);
+        // return qq.format("{}/{}", s3DemoGlobals.email, filename);
         // return qq.format("{}", filename);
+        return qq.format("{}", uuid);
       }
     },
     cors: {
@@ -88,31 +114,62 @@ $(function() {
       }
     },
     resume: {
-      enabled: true
+      enabled: true,
     },
     validation: {
       itemLimit: 20,
-      sizeLimit: 20*1073741824
+      sizeLimit: 20 * 1073741824,
     },
     maxConnections: 5,
-    // thumbnails: {
-    //   placeholders: {
-    //     notAvailablePath: "not_available-generic.png",
-    //     waitingPath: "waiting-generic.png"
-    //   }
-    // },
-    // callbacks: {
-    //     onAllComplete: function (e) {
-    //         window.alert("All complete. Notify Treehouse?");
-    //     }
-    // }
+    callbacks: {
+        onComplete: function (id, name, responseJSON, xhr) {
+          console.log("Finished uploading " + name);
+          tags = {
+            filename: name,
+            uid: this.getKey(id),
+            email: s3DemoGlobals.email,
+            name: s3DemoGlobals.name,
+          }
+          console.log(tags);
+
+            let s3 = new AWS.S3({region: "us-west-2"});
+						var params = {
+              Bucket: 'receiving-treehouse-ucsc-edu',
+							Key: this.getKey(id), 
+							Tagging: {
+							 TagSet: [
+									{
+								 Key: "filename", 
+								 Value: this.getName(id)
+								} 
+							 ]
+							}
+						 };
+             console.log(params);
+						 s3.putObjectTagging(params, function(err, data) {
+							 if (err) console.log(err, err.stack); // an error occurred
+							 else     console.log(data);           // successful response
+						 });
+            s3.putObject({
+                Bucket: 'receiving-treehouse-ucsc-edu',
+                Key: this.getKey(id) + ".json",
+                Body: JSON.stringify(tags, null, "  "),
+            }, function(err, data) {
+                if (err) console.log(err, err.stack); // an error occurred
+                else     console.log(data);           // successful response
+            });
+        },
+        onAllComplete: function (succeeded, failed) {
+            console.log("onAllComplete");
+            console.log(succeeded, failed);
+        }
+    }
   })
     .on('complete', function(event, id, name, response, xhr) {
       var $fileEl = $(this).fineUploaderS3("getItemByFileId", id),
         $viewBtn = $fileEl.find(".view-btn"),
         key = $(this).fineUploaderS3("getKey", id);
 
-      // Add a "view" button to access the uploaded file in S3 if the upload is successful
       if (response.success) {
         $viewBtn.show();
         $viewBtn.attr("href", bucketUrl + "/" + key);
