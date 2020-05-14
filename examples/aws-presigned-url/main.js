0f3f48573981cd7e8a835f944835273c4665d788
eajung <eriikjung@gmail.com>
2020-05-14 16:33:41 -0700

    updated backend of inbox, awaiting feedback

--- examples/aws-presigned-url/main.js
+++ examples/aws-presigned-url/main.js
@@ -1,27 +1,23 @@
 const grx = {}; // receiving globals
 var uuid='';
 // eslint-disable-next-line no-unused-vars
-function signIn(response) {
-  const profile = response.getBasicProfile();
-  grx.email = profile.getEmail();
-  grx.name = profile.getName();
-  console.log("User:", grx.email, grx.name);
+function signIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
 
-  $("#name").html(grx.name);
-  $("#email").html(grx.email);
-  // grx.assumeRoleWithWebIdentity({
-  //   roleArn: "arn:aws:iam::443872533066:role/browser-inbox-role",
-  //   idToken: response.getAuthResponse().id_token,
-  // });
-  document.cookie = "username="+grx.email;
-}
-
-// eslint-disable-next-line no-unused-vars
-function signOut() {
-  $("#fine-uploader").hide();
-  // eslint-disable-next-line no-undef
-  const auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(console.log("User signed out."));
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+  logged_in = 1;
+  console.log("Logged in status: " + logged_in);
+  window.email = profile.getEmail();
+  console.log("window -- grx.email : "+ window.email);
 }
 
 const Uppy = require('@uppy/core')
@@ -34,33 +30,46 @@ const uppy = Uppy({
 
 uppy.use(Dashboard, {
   inline: true,
-  target: 'body'
+  target: 'body',
+  metaFields: [
+      { id: 'name', name: 'Name', placeholder: 'file name' },
+      { id: 'caption', name: 'Caption', placeholder: 'describe what the image is about' }
+    ],
 })
+
 uppy.use(AwsS3, {
   getUploadParameters (file) {
-    // Send a request to our PHP signing endpoint.
-    return fetch('/s3-sign.php', {
-      method: 'post',
-      // Send and receive JSON.
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        filename: file.name,
-        contentType: file.type
+    file_name =  window.email + '/' + file.name;
+    console.log("Uploaded filename: " + file_name);
+    if (file.name.charAt(4) != '-') {
+      if (window.confirm('Please enter the UUID!\n\nIf you click "ok" you would be redirected to a UUID instruction page. Cancel will load this website ')) {
+        window.location.href='https://github.com/braingeneers/internal';
+      };
+    } else {
+      // Send a request to our PHP signing endpoint.
+      return fetch('/s3-sign.php', {
+        method: 'post',
+        // Send and receive JSON.
+        headers: {
+          accept: 'application/json',
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          filename: file_name,
+          contentType: file.type
+        })
+      }).then((response) => {
+        // Parse the JSON response.
+        return response.json()
+      }).then((data) => {
+        // Return an object in the correct shape.
+        return {
+          method: data.method,
+          url: data.url,
+          fields: data.fields,
+          headers: data.headers
+        }
       })
-    }).then((response) => {
-      // Parse the JSON response.
-      return response.json()
-    }).then((data) => {
-      // Return an object in the correct shape.
-      return {
-        method: data.method,
-        url: data.url,
-        fields: data.fields,
-        headers: data.headers
-      }
-    })
+    }
   }
 })
